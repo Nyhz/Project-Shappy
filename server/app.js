f546e36f493015fad081bcacc5a202707515dd10
dany <dacr63@gmail.com>
2021-10-08 09:15:12 +0200

    fixing bugs

--- server/app.js
+++ server/app.js
@@ -8,12 +8,12 @@ const app = express();
 require("./config")(app);
 require("./config/cors.config")(app);
 require('./config/session.config')(app)
-app.use(express.static(path.join(__dirname, "public")))
+// app.use(express.static(path.join(__dirname, "public")))
 
 
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
-app.use((req, res) => res.sendFile(__dirname + "/public/index.html"))
+// app.use((req, res) => res.sendFile(__dirname + "/public/index.html"))
 
 require("./error-handling")(app);
 
